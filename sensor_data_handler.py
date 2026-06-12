9a9726baa406bfc7ebf4aeef0f1c2e63e4d3f240
mohas95 <moha.debbagh95@gmail.com>
2026-06-12 14:00:06 -0400

    add class structure

--- sensor_data_handler.py
+++ sensor_data_handler.py
@@ -33,9 +33,76 @@ class SQLiteDataHandler:
         return conn
 
     def init_table(self, table_name, table_content):
+
+        if "logged_timestamp" not in table_content:
+            table_content += ", logged_timestamp TEXT NOT NULL"
+
+
         with self.connect_db() as conn:
             conn.execute(f"""
                 CREATE TABLE IF NOT EXISTS {table_name} (
                     {table_content}
                 )
             """)
+
+    def insert(self, table_name, data):
+        logged_timestamp = datetime.now(timezone.utc).isoformat()
+        data.setdefault("logged_timestamp", logged_timestamp)
+
+        ok, message = self.data_compatible(table_name, data)
+
+        if not ok:
+            raise ValueError(message)
+
+        data_keys = ", ".join(data.keys())
+        place_holders = ", ".join("?" for _ in data)
+        data_values = tuple(data.values())
+
+        with self.connect_db() as conn:
+            conn.execute(
+                f"""
+                INSERT INTO {table_name}
+                ({data_keys})
+                VALUES ({place_holders})
+                """,
+                data_values,
+            )
+
+    def data_compatible(self, table_name, data):
+        with self.connect_db() as conn:
+            rows = conn.execute(f"PRAGMA table_info({table_name})").fetchall()
+
+            if not rows:
+                raise ValueError(f"Table does not exist: {table_name}")
+            
+            columns ={
+                row["name"]:{
+                    "type": row["type"],
+                    "notnull": bool(row["notnull"]),
+                    "default": row["dflt_value"],
+                    "pk": bool(row["pk"]),
+                }
+
+                for row in rows
+            }
+
+            data_keys = set(data.keys())
+            column_keys = set(columns.keys())
+
+            extra_keys = data_keys-column_keys
+            if extra_keys:
+                return False, f"Extra columns not in table: {extra_keys}"
+            
+            missing_required = []
+
+            for  col_name, info in columns.items():
+                is_primary_key = info["pk"]
+                has_default = info["default"] is not None
+
+                if(info["notnull"] and not has_default and not is_primary_key and col_name not in data):
+                    missing_required.append(col_name)
+
+            if missing_required:
+                return False, f"Missing required columns: {missing_required}"
+            
+            return True, "compatible"
